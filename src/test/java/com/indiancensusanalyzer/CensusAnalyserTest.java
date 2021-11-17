4945509dcca983c15c255edc4ba73ef45a42e23b
MKS1998 <nksayyad2900@outlook.com>
2021-11-17 18:01:45 +0530

    Added : Analyzer to load indian state census information from csv

---
+++ src/test/java/com/indiancensusanalyzer/CensusAnalyserTest.java
@@ -0,0 +1,18 @@
+package com.indiancensusanalyzer;
+
+import com.bridgelabz.indianstatescensusanalyser.model.CensusAnalyser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class CensusAnalyserTest {
+	private String INIDAN_CENSUS_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+	@Test
+	public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
+		CensusAnalyser censusAnalyser = new CensusAnalyser();
+		int count = censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_CSV_FILE_PATH);
+		Assert.assertEquals(29, count);
+	}
+}
